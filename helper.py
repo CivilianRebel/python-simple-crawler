853b5626b5d720d2ab063a05a626cc450e51d2db
CivilianRebel <steventayloroo@gmail.com>
2018-09-22 02:43:40 -0400

    bugfix

--- helper.py
+++ helper.py
@@ -9,11 +9,12 @@ def split(arr, n_chunks):
         yield arr[i:i + n_chunks]
 
 
+# testing the link parsing
 c = instance.Crawler(1, 10, True)
 url = 'https://slate.com/culture/2018/09/tiger-woods-2018-bmw-championship.html'
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html, c.parser)
 links = [link.get('href') for link in soup.find_all('a')]
-links = [c.parse_urls(link, url) for link in links]
+links = [c.parse_url(link, url) for link in links]
 [print(parse_url(link).host, '\n') for link in links]
